0ee82f90b40b85f3071f23b98598a3c98cc88c5a
islador <luke.isla@gmail.com>
2014-03-26 13:32:29 -0700

    Initial commit! Woo! Finally starting Ananke

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ananke::Application.config.secret_key_base = '37b92eb97ac50d6c32b6038c53dafe2d32ef165f8271039f54eac7276b61b59ede8b5a7393942c837eefa863e281538b177f1b22bb3337e880d79a8b4a30811f'
