cec346bef15e4d3fc5f495b652f230e39e83e89a
islador <luke.isla@gmail.com>
2014-03-27 00:10:04 -0700

    Added a basic API model, almost identical to the one used in market_monitor. It seems reasonable to copy certain comopnents of the code from market_monitor given the similarity in the API interactions.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140327032429) do
+ActiveRecord::Schema.define(version: 20140327070910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "apis", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "entity"
+    t.string   "key_id"
+    t.string   "v_code"
+    t.integer  "accessmask"
+    t.boolean  "active"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
