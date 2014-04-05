9a804e8c3cf2644ef93955b92637c70a80b3e888
islador <luke.isla@gmail.com>
2014-04-05 00:59:04 -0700

    Removed home.html.haml as it is likely not going to be used. Added a devise redirect to the application_controller that now redirects signed in users to white_list.haml. This should be changed to the API pages once they're up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(user)
+  	#This needs to be changed to link to the APIs pages once they're up.
+	  whitelist_white_list_path
+	end
 end
