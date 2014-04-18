2fb11fd60e2d21f60121448f5e2fed8a3ff486f5
islador <luke.isla@gmail.com>
2014-04-18 16:16:22 -0700

    Discoverd intermittent VCR cassette matching failures in view specs, specifically api/new.html.haml. The intermittency was caused by the use of JS tests via selenium, apparently these are executed in a multithreaded environment. To fix the problem, cassettes are loaded at the beginning and end of each test utilizing one. This seems to have solved the issue by allowing padding time before and after the matching event.

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -3,8 +3,11 @@ require 'vcr'
 VCR.configure do |c|
 	c.allow_http_connections_when_no_cassette = false
 	c.ignore_localhost = true
+	c.debug_logger = File.open(ARGV.first, 'w')
 	c.cassette_library_dir = 'spec/vcr_cassettes'
 	c.hook_into :webmock
 	c.configure_rspec_metadata!
-	c.default_cassette_options = { :record => :once, :erb => true }
+	c.default_cassette_options = {
+    :match_requests_on => [:method, :path, :host], :record => :once, :erb => true
+  }
 end
--- spec/views/api/new.html.haml_spec.rb
+++ spec/views/api/new.html.haml_spec.rb
@@ -33,21 +33,26 @@ describe "api/new.html.haml > " do
 	end
 
 	describe "First API > " do
+		
 		it "should have the main api box checked by default" do
 			find('input#main_api').should be_checked
-			fill_in('key_id', :with => "3255235")
-			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-			click_button 'Enroll Key'
 		end
 
 		it "should uncheck the main api box after a successful enrollment", js: true do
-			fill_in('key_id', :with => "3255235")
-			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-			click_button 'Enroll Key'
-			find('input#main_api').should_not be_checked
+			VCR.use_cassette('workers/api_key_info/characterAPI') do
+				fill_in('key_id', :with => "3255235")
+				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+			
+				click_button 'Enroll Key'
+				find('input#main_api').should_not be_checked
+			end
 		end
 
-		let!(:api) {FactoryGirl.create(:api, user: user)}
+		let!(:api) {
+			VCR.use_cassette('workers/api_key_info/characterAPI') do
+				FactoryGirl.create(:api, user: user)
+			end
+		}
 		it "should not have the main api box checked if it is not the user's first API" do
 			visit new_user_api_path(user)
 			find('input#main_api').should_not be_checked
@@ -56,115 +61,56 @@ describe "api/new.html.haml > " do
 
 	describe "Main API > " do
 		it "after a main api is submitted, it should lock the screen for five seconds", js: true do
-			fill_in('key_id', :with => "3255235")
-			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-			find(:css, "#main_api").set(true)
-			click_button 'Enroll Key'
-
-			should have_selector("div.modal-backdrop")
+			VCR.use_cassette('workers/api_key_info/characterAPI') do
+				fill_in('key_id', :with => "3255235")
+				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+				find(:css, "#main_api").set(true)
+			
+				click_button 'Enroll Key'
+				should have_selector("div.modal-backdrop")
+			end
 		end
 		it "after a main API is submitted, it should load in the character list partial", js: true do
-			fill_in('key_id', :with => "3255235")
-			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-			find(:css, "#main_api").set(true)
-			click_button 'Enroll Key'
+			VCR.use_cassette('workers/api_key_info/characterAPI') do
+				fill_in('key_id', :with => "3255235")
+				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+				find(:css, "#main_api").set(true)
+			
+				click_button 'Enroll Key'
 
-			should have_selector('div#character_list')
-			should have_selector('div.explanation')
-			should have_selector('table#character_list_table')
-			#should have_selector("tr", text: "Tany Ishsar")
-			#should have_selector("button", text: "Set as Main")
-		end
-
-		describe "Character List Partial > " do
-			#it "should contain an explanation of terms" do
-			#	fill_in('key_id', :with => "3255235")
-			#	fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-			#	find(:css, "#main_api").set(true)
-			#	click_button 'Enroll Key'
-
-			#	should have_selector('div.explanation')
-			#end
-
-			describe "Character List Table > " do
-				#it "should render the character_list table" do
-				#	fill_in('key_id', :with => "3255235")
-				#	fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-				#	find(:css, "#main_api").set(true)
-				#	click_button 'Enroll Key'
-
-				#	should have_selector('table#character_list_table')
-				#end
-
-				#it "should render datatables", js: true do
-				#	fill_in('key_id', :with => "3255235")
-				#	fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-				#	find(:css, "#main_api").set(true)
-				#	click_button 'Enroll Key'
-
-				#	should have_selector('#character_list_table_wrapper')
-				#end
-
-				#it "should contain the api's characters" do
-				#	fill_in('key_id', :with => "3255235")
-				#	fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-				#	find(:css, "#main_api").set(true)
-				#	click_button 'Enroll Key'
-
-				#	within 'table#character_list_table' do
-				#		should have_selector("tr", text: "Tany Ishsar")
-						#should have_selector("tr#character_id_#{character2.id}")
-						#should have_selector("tr#character_id_#{character3.id}")
-				#	end
-				#end
-
-				#it "should not contain other api's characters" do
-				#	fill_in('key_id', :with => "3255235")
-				#	fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-				#	find(:css, "#main_api").set(true)
-				#	click_button 'Enroll Key'
-
-				#	within 'table#character_list_table' do
-				#		should_not have_selector("tr", text: "islador")
-				#	end
-				#end
-
-				#it "if no characters are set as the main, it should contain a button to set a character as the main", js: true do
-				#	fill_in('key_id', :with => "3255235")
-				#	fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-				#	find(:css, "#main_api").set(true)
-				#	click_button 'Enroll Key'
-
-				#	should have_selector("button", text: "Set as Main")
-					#should have_selector("button", text: "Set as Main")
-					#should have_selector("button", text: "Set as Main")
-				#end
+				should have_selector('div#character_list')
+				should have_selector('div.explanation')
+				should have_selector('table#character_list_table')
 			end
 		end
 	end
 
 	describe "Create > " do
 		it "should add the API to the database", js: true do
-			fill_in('key_id', :with => "3255235")
-			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+			VCR.use_cassette('workers/api_key_info/characterAPI') do
+				fill_in('key_id', :with => "3255235")
+				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
 
-			find_field('key[id]').value.should eq '3255235'
-			find_field('v[code]').value.should eq 'P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x'
+				find_field('key[id]').value.should eq '3255235'
+				find_field('v[code]').value.should eq 'P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x'
 
-			count = Api.count
-
-			click_button 'Enroll Key'
-			sleep(6)
-			Api.count.should be > count
+				count = Api.count
+			
+				click_button 'Enroll Key'
+				
+				sleep(6)
+				Api.count.should be > count
+			end	
 		end
 
 		it "should clear the v_code and key_id fields after creating an API", js: true do
-			fill_in('key_id', :with => "3255235")
-			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-			click_button 'Enroll Key'
-
-			find_field('key[id]').value.should eq ''
-			find_field('v[code]').value.should eq ''
+			VCR.use_cassette('workers/api_key_info/characterAPI') do
+				fill_in('key_id', :with => "3255235")
+				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+				click_button 'Enroll Key'
+				find_field('key[id]').value.should eq ''
+				find_field('v[code]').value.should eq ''
+			end
 		end
 	end
 end
