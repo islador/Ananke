19ddf2c4380359297fdad13379e60c82340cf9c9
islador <luke.isla@gmail.com>
2014-04-24 00:29:52 -0700

    Nightly full test revealed some intermittent failures. They appear to be sidekiq testing errors, because when isolated, the tests are green. This is further backed by the failed specs triggering VCR errors even when sidekiq is set to fake, thus preventing any execution. The current theory is that sidekiq is not swapping settings between specs, thus causing some to run inline even when they should be running faked.

--- spec/views/whitelist/white_list.haml_spec.rb
+++ spec/views/whitelist/white_list.haml_spec.rb
@@ -78,19 +78,23 @@ describe "whitelist/white_list.haml > " do
 			end
 
 			it "when 'Query API' is clicked it should remove the api from the table", js: true do
-				click_button 'Begin New API Pull'
-		        click_button 'Query API'
-		        should_not have_selector("tr#add_api_#{valid_api.id}")
+				VCR.use_cassette('workers/corpContactList_standingsSpread') do
+					click_button 'Begin New API Pull'
+			        click_button 'Query API'
+			        should_not have_selector("tr#add_api_#{valid_api.id}")
+				end
 			end
 
 			it "When 'Query API' is clicked, it should add the api to the api_pulls_table", js: true do
-				click_button 'Begin New API Pull'
-		        click_button 'Query API'
-		        within '#api_pulls_table' do
-					should have_selector("tr td", text: "You")
-					should have_selector("tr td", text: valid_api.main_entity_name)
-					should have_selector("tr td", text: "10")
-					should have_selector("tr td", text: valid_api.key_id)
+				VCR.use_cassette('workers/corpContactList_standingsSpread') do
+					click_button 'Begin New API Pull'
+			        click_button 'Query API'
+			        within '#api_pulls_table' do
+						should have_selector("tr td", text: "You")
+						should have_selector("tr td", text: valid_api.main_entity_name)
+						should have_selector("tr td", text: "10")
+						should have_selector("tr td", text: valid_api.key_id)
+					end
 				end
 			end
 		end
