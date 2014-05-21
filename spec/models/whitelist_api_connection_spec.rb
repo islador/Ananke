a42ed4ccd5a5dfad0a5691757223d7546aad4b73
islador <luke.isla@gmail.com>
2014-05-21 10:42:15 -0700

    Updated whitelist_api_spec to use the updated share_user factories. ALl model specs are now green. Next step is to move up to controller specs.

--- spec/models/whitelist_api_connection_spec.rb
+++ spec/models/whitelist_api_connection_spec.rb
@@ -15,20 +15,21 @@ Sidekiq::Testing.inline!
 
 describe WhitelistApiConnection do
 	let!(:user) {FactoryGirl.create(:user)}
+	let!(:share_user) {FactoryGirl.create(:share_user, user_id: user.id)}
 	let!(:api) {
 		VCR.use_cassette('workers/api_key_info/characterAPI') do
-			FactoryGirl.create(:corp_api, user: user)
+			FactoryGirl.create(:corp_api, share_user: share_user)
 		end
 	}
-	let!(:whitelist_entity) {FactoryGirl.create(:whitelist, source_type: 1, source_user: user.id)}
+	let!(:whitelist_entity) {FactoryGirl.create(:whitelist, source_type: 1, source_share_user: share_user.id)}
 	let!(:whitelist_api_connection) {FactoryGirl.create(:whitelist_api_connection, api_id: api.id, whitelist_id: whitelist_entity.id)}
 
 	let!(:api2) {
 		VCR.use_cassette('workers/api_key_info/characterAPI') do
-			FactoryGirl.create(:corp_api, user: user)
+			FactoryGirl.create(:corp_api, share_user: share_user)
 		end
 	}
-	let!(:whitelist_entity2) {FactoryGirl.create(:whitelist, source_type: 1, source_user: user.id)}
+	let!(:whitelist_entity2) {FactoryGirl.create(:whitelist, source_type: 1, source_share_user: share_user.id)}
 	let!(:whitelist_api_connection_api2_whitelist_entity2) {FactoryGirl.create(:whitelist_api_connection, api_id: api2.id, whitelist_id: whitelist_entity2.id)}
 	let!(:whitelist_api_connection_api_whitelist_entity2) {FactoryGirl.create(:whitelist_api_connection, api_id: api.id, whitelist_id: whitelist_entity2.id)}
 
