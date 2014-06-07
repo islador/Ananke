07ec051dd6aff47c6d56dff4d0f11842db5e9fc4
islador <luke.isla@gmail.com>
2014-06-07 14:23:39 -0700

    Updated the whitelist_log.haml specs and page to support the new share_users and associated routing scheme. Specs are now green.

--- spec/views/whitelist/white_list_log.haml_spec.rb
+++ spec/views/whitelist/white_list_log.haml_spec.rb
@@ -2,21 +2,24 @@ require 'spec_helper'
 
 describe "whitelist/white_list_log.haml >" do
 	subject {page}
-	let!(:user) {FactoryGirl.create(:user)}
+	let(:user) {FactoryGirl.create(:user)}
+	let(:share) {FactoryGirl.create(:share)}
+	let!(:share_user) {FactoryGirl.create(:share_user, user_id: user.id, share_id: share.id)}
 
 
 	before(:each) do
-		visit whitelist_white_list_log_path
+		visit share_user_whitelist_white_list_log_path(share_user)
 		fill_in('user_email', :with => user.email)
 		fill_in('user_password', :with => user.password)
 		click_button 'Sign in'
-		visit whitelist_white_list_log_path
+		find("#share_#{share.id}").click
+		visit share_user_whitelist_white_list_log_path(share_user)
 	end
 
 	describe " Table > " do
 		let!(:whitelistlog) {FactoryGirl.create(:whitelist_log, entity_name: "Jeff")}
 
-		it "should render the white_list table" do
+		it "should render the white_list table", js: true do
 			should have_selector('#whitelist_log_table')
 		end
 		
@@ -25,7 +28,7 @@ describe "whitelist/white_list_log.haml >" do
 		end
 
 		it "should contain items from the database", js: true do
-			visit whitelist_white_list_log_path
+			visit share_user_whitelist_white_list_log_path(share_user)
 			within '#whitelist_log_table' do
 				should have_selector("tr#entity_#{WhitelistLog.last.id}", text: whitelistlog.entity_name)
 			end
