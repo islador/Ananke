f9017a34d745e8c00942278e48cb986727b1cac9
islador <luke.isla@gmail.com>
2014-07-04 17:40:24 -0700

    Updated views/api/new spec to handle the new active nil default factory. Hardened it against order dependency failures. It is green under most seeds when run independtly, and under 29206 when run with all the specs.

--- spec/views/api/new.html.haml_spec.rb
+++ spec/views/api/new.html.haml_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
-require 'sidekiq/testing'
-Sidekiq::Testing.inline!
+#require 'sidekiq/testing'
+#Sidekiq::Testing.inline!
 
 describe "api/new.html.haml > " do
 	let(:user) {FactoryGirl.create(:user)}
@@ -9,7 +9,6 @@ describe "api/new.html.haml > " do
 	Capybara.default_wait_time = 10
 
 	subject {page}
-	
 
 	before(:each) do
 		visit new_share_user_api_path(share_user)
@@ -32,163 +31,177 @@ describe "api/new.html.haml > " do
 	end
 
 	describe "First API > " do
+		before(:each) do
+			VCR.insert_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1})
+		end
+		after(:each) do
+			VCR.eject_cassette()
+		end
 
 		it "should have the main api box checked by default", js: true do
 			find('input#main_api').should be_checked
 		end
 
 		it "should uncheck the main api box after a successful enrollment", js: true do
-			VCR.use_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount+=1}) do
-				fill_in('key_id', :with => Rails.configuration.charCount)
-				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-			
-				click_button 'Enroll Key'
-				sleep(1)
-				find('input#main_api').should_not be_checked
-				#should have_selector("div.modal-backdrop")
-			end
+			fill_in('key_id', :with => Rails.configuration.charCount)
+			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+		
+			click_button 'Enroll Key'
+			sleep(1)
+			find('input#main_api').should_not be_checked
+			#should have_selector("div.modal-backdrop")
 		end
 	end
-
 	describe "Additional APIs > " do
-		let!(:api) {
-			VCR.use_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "alex", :charID => Rails.configuration.charCount+=1}) do
-				FactoryGirl.create(:api, share_user: share_user)
-			end
-		}
+		before(:each) do
+			FactoryGirl.create(:character_api_skip_determine_type, share_user: share_user)
+		end
 
 		it "should not have the main api box checked if it is not the user's first API", js: true do
 			visit new_share_user_api_path(share_user)
 			find('input#main_api').should_not be_checked
 		end
+	end
+
+	describe "Corp APIs > " do
+		before(:each) do
+			VCR.insert_cassette('workers/api_key_info/dynamicCorpAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1})
+		end
+		after(:each) do
+			VCR.eject_cassette()
+		end
 
 		it "should redirect to the share user's api index if a corp api is enrolled", js: true do
-			VCR.use_cassette('workers/api_key_info/dynamicCorpAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount+=1}) do
-				fill_in('key_id', :with => Rails.configuration.charCount)
-				fill_in('v_code', :with => "UyO6KSsDydLrZX7MwU048rqRiHwAexvLmSQgtiUbN0rIrVaUuGUZYmGuW2PkMSg1")
-			
-				click_button 'Enroll Key'
-				should have_selector("h3", text: 'Your APIs')
-			end
+			fill_in('key_id', :with => "3255235")
+			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+		
+			click_button 'Enroll Key'
+
+			should have_selector("h3", text: 'Your APIs')
 		end
 	end
 
 	describe "Main API > " do
-		it "after a main api is submitted, it should lock the screen for five seconds", js: true do
-			VCR.use_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1}) do
-				fill_in('key_id', :with => "3255235")
-				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-				find(:css, "#main_api").set(true)
-			
-				click_button 'Enroll Key'
-				should have_selector("div.modal-backdrop")
-			end
-		end
-		it "after a main API is submitted, it should load in the character list partial", js: true do
-			VCR.use_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1}) do
-				fill_in('key_id', :with => "3255235")
-				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-				find(:css, "#main_api").set(true)
-			
-				click_button 'Enroll Key'
 
-				should have_selector('div#character_list')
-				should have_selector('div.explanation')
-				should have_selector('table#character_list_table')
-			end
+		before(:each) do
+			VCR.insert_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1})
+		end
+		after(:each) do
+			VCR.eject_cassette()
+		end
+
+		it "after a main api is submitted, it should lock the screen for five seconds", js: true do
+			fill_in('key_id', :with => "3255235")
+			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+			find(:css, "#main_api").set(true)
+		
+			click_button 'Enroll Key'
+			should have_selector("div.modal-backdrop")
+		end
+
+		it "after a main API is submitted, it should load in the character list partial", js: true do
+			fill_in('key_id', :with => "3255235")
+			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+			find(:css, "#main_api").set(true)
+		
+			click_button 'Enroll Key'
+
+			should have_selector('div#character_list')
+			should have_selector('div.explanation')
+			should have_selector('table#character_list_table')
 		end
 
 		it "after a main API is submitted, it should load in the character list partial and allow users to immediately set a main character.", js: true do
-			VCR.use_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1}) do
-				fill_in('key_id', :with => "3255235")
-				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-				find(:css, "#main_api").set(true)
-			
-				click_button 'Enroll Key'
+			fill_in('key_id', :with => "3255235")
+			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+			find(:css, "#main_api").set(true)
+		
+			click_button 'Enroll Key'
 
-				should have_selector('div#character_list')
-				should have_selector('div.explanation')
-				should have_selector('table#character_list_table')
+			should have_selector('div#character_list')
+			should have_selector('div.explanation')
+			should have_selector('table#character_list_table')
 
-				should have_content('button', :text => "Set as Main")
-			end
+			should have_content('button', :text => "Set as Main")
 		end
 
 		it "after a main API's main character is selected, it should redirect to the share user's api index", js: true do
-			VCR.use_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1}) do
-				fill_in('key_id', :with => "3255235")
-				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-				find(:css, "#main_api").set(true)
-			
-				click_button 'Enroll Key'
+			fill_in('key_id', :with => "3255235")
+			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+			find(:css, "#main_api").set(true)
+		
+			click_button 'Enroll Key'
 
-				should have_selector('div#character_list')
-				should have_selector('div.explanation')
-				should have_selector('table#character_list_table')
+			should have_selector('div#character_list')
+			should have_selector('div.explanation')
+			should have_selector('table#character_list_table')
 
-				#http://stackoverflow.com/a/2609244
-				page.evaluate_script('window.confirm = function() { return true; }')
-				click_button 'Set as Main'
-				should have_selector('h3', text: "Your APIs")
-			end
+			#http://stackoverflow.com/a/2609244
+			page.evaluate_script('window.confirm = function() { return true; }')
+			click_button 'Set as Main'
+			should have_selector('h3', text: "Your APIs")
 		end
 
 		it "Clicking 'Set as Main' should set that character as the main character", js: true do
-			VCR.use_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1}) do
-				fill_in('key_id', :with => "3255235")
-				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-				find(:css, "#main_api").set(true)
-			
-				click_button 'Enroll Key'
+			fill_in('key_id', :with => "3255235")
+			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+			find(:css, "#main_api").set(true)
+		
+			click_button 'Enroll Key'
 
-				#http://stackoverflow.com/a/2609244
-				page.evaluate_script('window.confirm = function() { return true; }')
-				click_button 'Set as Main'
-				Character.last.main.should be true
-			end
+			#http://stackoverflow.com/a/2609244
+			page.evaluate_script('window.confirm = function() { return true; }')
+			click_button 'Set as Main'
+			Character.last.main.should be true
 		end
 	end
 
 	describe "Non Main API > " do
+		before(:each) do
+			VCR.insert_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1})
+		end
+		after(:each) do
+			VCR.eject_cassette()
+		end
 		it "after non-main api is enrolled, it should redirect to the share user's api index", js: true do
-			VCR.use_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1}) do
-				fill_in('key_id', :with => "3255235")
-				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-				find(:css, "#main_api").set(false)
-			
-				click_button 'Enroll Key'
+			fill_in('key_id', :with => "3255235")
+			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+			find(:css, "#main_api").set(false)
+		
+			click_button 'Enroll Key'
 
-				should have_selector('h3', text: "Your APIs")
-			end
+			should have_selector('h3', text: "Your APIs")
 		end
 	end
 
 	describe "Create > " do
+		before(:each) do
+			VCR.insert_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1})
+		end
+		after(:each) do
+			VCR.eject_cassette()
+		end
 		it "should add the API to the database", js: true do
-			VCR.use_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1}) do
-				fill_in('key_id', :with => "3255235")
-				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+			fill_in('key_id', :with => "3255235")
+			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
 
-				find_field('key[id]').value.should eq '3255235'
-				find_field('v[code]').value.should eq 'P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x'
+			find_field('key[id]').value.should eq '3255235'
+			find_field('v[code]').value.should eq 'P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x'
 
-				count = Api.count
+			count = Api.count
+		
+			click_button 'Enroll Key'
 			
-				click_button 'Enroll Key'
-				
-				sleep(6)
-				Api.count.should be > count
-			end	
+			sleep(6)
+			Api.count.should be > count	
 		end
 
 		it "should clear the v_code and key_id fields after creating an API", js: true do
-			VCR.use_cassette('workers/api_key_info/dynamicCharacterAPI', erb: {:charName => "#{Rails.configuration.charCount+1}", :charID => Rails.configuration.charCount += 1}) do
-				fill_in('key_id', :with => "3255235")
-				fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
-				click_button 'Enroll Key'
-				find_field('key[id]').value.should eq ''
-				find_field('v[code]').value.should eq ''
-			end
+			fill_in('key_id', :with => "3255235")
+			fill_in('v_code', :with => "P4IZDKR0BqaFVZdvy24QVnFmkmsNjcicEocwvTdpxtTz7YhF2tPNigeVhr3Y8l5x")
+			click_button 'Enroll Key'
+			find_field('key[id]').value.should eq ''
+			find_field('v[code]').value.should eq ''
 		end
 	end
 end
