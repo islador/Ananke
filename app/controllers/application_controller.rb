ed0a6dea2b6ed0b4b83e7f4219856abfe58b808c
islador <luke.isla@gmail.com>
2014-07-12 19:47:58 -0700

    Removed legacy code and comments from the applicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,25 +23,16 @@ class ApplicationController < ActionController::Base
 		session[:previous_url] || share_index_path(current_user)
 	end
 
-	#def after_sign_in_path_for(user)
-	#	current_user_path
-		#share_index_path(current_user)
-	#end
-
 	#I dislike this implementation. I would prefer to use two seperate functions with the same name and different args.
 	#However, that throws errors on some specs.
 	def current_share_user(share_id=nil)
 		if share_id.nil? == true
-			#This currently offers no security. Any user may change URLs to view another's data.
-			#@current_share_user = ShareUser.where("id = ?", params[:share_user_id])[0]
-			
 			#This implementation locks the return to a given user, which prevents other users from seeing another's data
 			@current_share_user = ShareUser.where("id = ? AND user_id = ?", params[:share_user_id], current_user.id)[0]
 			
 			#The below may not be needed; more research is required to determine this.
 			if @current_share_user.nil? == true
 				#Using this method as the primary adds security by returning nil if the user does not actually possess the share_user given as a param.
-				#puts "invoking fallback"
 				@current_share_user = ShareUser.where("share_id = ? AND user_id = ?", params[:id], current_user.id)[0]
 			end
 
@@ -54,7 +45,6 @@ class ApplicationController < ActionController::Base
 
 	def require_share_user
 		if current_share_user == nil
-		#if session[:share_user_id] == nil
 			flash[:error] = "You either lack permission to view that page, or have not selected a group yet. Please select a group before continuing."
 			redirect_to share_index_path
 		end
