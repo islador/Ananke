0dd29ac26e2c1ee404b5fd80854a1c2dc94876de
islador <luke.isla@gmail.com>
2014-07-13 16:36:53 -0700

    Updated the respect_share_validator to deliver a more useful error message.

--- app/validators/respect_share_validator.rb
+++ app/validators/respect_share_validator.rb
@@ -2,10 +2,9 @@ class RespectShareValidator < ActiveModel::Validator
 	def validate(record)
 		#Required to ensure that the presence validation spec for share_id doesn't trigger this validation to throw a nil error.
 		return if record.share_id.nil?
-
 		#This simply calls the record's owning share and then passes the record to it.
 		if record.share.respect_share?(record) == false
-			record.errors.add(:share_users, 'The share has reached its user limit, please contact your CEO or Tech Admin about this issue.')
+			record.errors.add(:share_users, "There are no more available slots. #{record.main_char_name} has been notified of your attempt to join and the problem. Your account will be approved when the problem is fixed.")
 		end
 	end
 end
