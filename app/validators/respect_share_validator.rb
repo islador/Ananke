75cfd58ae93c14bed4e54f53ad95a074268b9b15
islador <luke.isla@gmail.com>
2014-07-13 21:58:08 -0700

    respect_share_validator: Added a trigger for ShareMailer.delay.user_limit_exceed(record) to the respect_share_validator. This may pose a problem, however since it's using sidekiq, I doubt it.

--- app/validators/respect_share_validator.rb
+++ app/validators/respect_share_validator.rb
@@ -5,6 +5,7 @@ class RespectShareValidator < ActiveModel::Validator
 		#This simply calls the record's owning share and then passes the record to it.
 		if record.share.respect_share?(record) == false
 			record.errors.add(:share_users, "There are no more available slots. #{record.main_char_name} has been notified of your attempt to join and the problem. Your account will be approved when the problem is fixed.")
+			ShareMailer.delay.user_limit_exceeded(record)
 		end
 	end
 end
