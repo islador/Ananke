114b4f7f0566d21e8f9280db6907cfd8844b02ff
islador <luke.isla@gmail.com>
2014-07-16 13:43:15 -0700

    Transfered generate_addition and generate_removal log methods from whitelist to blacklistentity and supporting after_destroy and after_save callbacks. The BlackListEntity specs are now green and log creation occurs as intended.

--- app/models/black_list_entity.rb
+++ app/models/black_list_entity.rb
@@ -24,4 +24,20 @@ class BlackListEntity < ActiveRecord::Base
 	#The below validations are linked, ideally a custom validation should be made that ensures that the whitelist and the share_user belong to the same share.
 	validates :source_share_user_id, presence: true
 	validates :share_id, presence: true
+
+	after_destroy :generate_removal_log
+	after_save :generate_addition_log
+
+	private
+	#Creates a new whitelist log record representing itself being created.
+	#This can be moved to sidekiq if necessary.
+	def generate_addition_log
+		BlackListEntityLog.create(entity_name: self.name, addition: true, entity_type: self.entity_type, source_type: self.source_type, source_share_user_id: self.source_share_user_id, date: Date.today, time: Time.now, share_id: self.share_id)
+	end
+
+	#Creates a new whitelist log record representing itself being destroyed.
+	#This can be moved to sidekiq if necessary.
+	def generate_removal_log
+		BlackListEntityLog.create(entity_name: self.name, addition: false, entity_type: self.entity_type, source_type: self.source_type, source_share_user_id: self.source_share_user_id, date: Date.today, time: Time.now, share_id: self.share_id)
+	end
 end
