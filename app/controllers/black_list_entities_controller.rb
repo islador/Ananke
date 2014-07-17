6ee46d50110b7c81deb678ae8f5bc7daf9a7e85c
islador <luke.isla@gmail.com>
2014-07-17 11:24:08 -0700

    Built out the black_list_entity_controller#log method. Its specs are now green.

--- app/controllers/black_list_entities_controller.rb
+++ app/controllers/black_list_entities_controller.rb
@@ -1,21 +1,32 @@
 class BlackListEntitiesController < ApplicationController
-  before_action :authenticate_user!, :require_share_user
-  
-  def create
-  end
+    before_action :authenticate_user!, :require_share_user
 
-  def destroy
-  end
+    def create
+    end
 
-  def index
-  end
+    def destroy
+    end
 
-  def show
-  end
+    def index
+    end
 
-  def logs
-  end
+    def show
+    end
 
-  def retrieve_pullable_apis
-  end
+    def logs
+        csu = current_share_user
+        @bll = BlackListEntityLog.where("share_id = ?", csu.share_id)
+
+        @source_share_user_names = {}
+
+        values = BlackListEntityLog.where("share_id =?", csu.share_id).pluck("source_share_user_id").uniq
+        names = ShareUser.where(id: values).pluck("main_char_name")
+        values.each do |val|
+            @source_share_user_names.store(names[values.index(val)].to_sym, val)
+        end
+
+    end
+
+    def retrieve_pullable_apis
+    end
 end
