9ab56b1eaf90bbd046d6d378d1717f2ccdc3e62f
islador <luke.isla@gmail.com>
2014-07-17 00:54:28 -0700

    controller/black_list_entity_controller_spec: Built out specs that test whether the #log method retrieves the correct black_list_entity_log records and retrieves the correct share_user.main_char_name's associated with the black_list_entity_log.source_share_user_id.

--- spec/controllers/black_list_entities_controller_spec.rb
+++ spec/controllers/black_list_entities_controller_spec.rb
@@ -10,11 +10,12 @@ describe BlackListEntitiesController do
   let!(:user) {FactoryGirl.create(:user)}
   let(:share) {FactoryGirl.create(:share)}
   let!(:share_user) {FactoryGirl.create(:share_user, share_id: share.id, user_id: user.id)}
-  let!(:blacklist) {FactoryGirl.create(:black_list_entity, share_id: share.id, source_share_user_id: user.id)}
+  let!(:blacklist) {FactoryGirl.create(:black_list_entity, share_id: share.id, source_share_user_id: share_user.id)}
 
   let(:share2) {FactoryGirl.create(:share)}
   let(:user2) {FactoryGirl.create(:user)}
   let!(:share_user2) {FactoryGirl.create(:share_user, share_id: share2.id, user_id: user2.id)}
+  let!(:blacklist2) {FactoryGirl.create(:black_list_entity, share_id: share2.id, source_share_user_id: share_user2.id)}
 
   describe "GET 'create'" do
     before(:each){
@@ -63,6 +64,20 @@ describe BlackListEntitiesController do
     it "returns http success" do
       get 'logs', share_user_id: share_user.id
       response.should be_success
+    end
+
+    let!(:black_list_entity_log) {FactoryGirl.create(:black_list_entity_log, share_id: share.id)}
+    let!(:black_list_entity_log2) {FactoryGirl.create(:black_list_entity_log, share_id: share2.id)}
+    it "should build an @bll object containing all whitelists in the current share" do
+      get 'logs', share_user_id: share_user.id
+      expect(assigns(:bll)).to include(black_list_entity_log)
+      expect(assigns(:bll)).to_not include(black_list_entity_log2)
+    end
+
+    it "should build an @user_char_names object containing all of the source_share_user_id's main_char_names" do
+      get 'logs', share_user_id: share_user.id
+      expect(assigns(:user_char_names)[0]).to match "#{share_user.main_char_name}"
+      expect(assigns(:user_char_names)).to_not include("#{share_user2.main_char_name}")
     end
   end
 
